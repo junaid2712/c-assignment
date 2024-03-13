0c9d8815cf5c27190de04e62bbe2f36994cb4572
MOHMMEDJUNAID <junaid.ece.rymec@gmail.com>
2024-03-13 15:10:29 +0530

    assignpg2.c

---
+++ assignpg2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the integer number \n");
+	scanf("%d",&a);
+
+	printf("the integer value in character  is %c\n",a);
+	printf("the integer value in octal  is %o\n",a);
+	printf("the integer value in hexa  is %x\n",a);
+
+	return  0;
+}
